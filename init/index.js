6a740cbc1bd2104f82334e12efd01f64d83ad897
Alok Chandra <alokchandra2611@gmail.com>
2026-04-05 09:07:29 +0530

    Fix dependencies, update seeding scripts, and add start command

--- init/index.js
+++ init/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb+srv://alokchandra:alokchandra%4026@bloodwork.nhqcfbs.mongodb.net/StayZen?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URI;
+
+const User = require("../models/user.js");
 
 main().then(() => {
     console.log("Connected to the DB")
@@ -16,9 +19,19 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "6814dcc324345c9de42124f2"}))
+    
+    // Fetch a real user to be the owner
+    const user = await User.findOne({ username: "AlokChandra" });
+    const ownerId = user ? user._id : "68751711b5751b714bf21d67";
+
+    initData.data = initData.data.map((obj) => ({
+        ...obj, 
+        owner: ownerId,
+        geometry: { type: "Point", coordinates: [0, 0] } // Default geometry to bypass validation
+    }));
+    
     await Listing.insertMany(initData.data);
-    console.log("Data was initialized");
+    console.log("Data was initialized successfully with 30+ listings.");
 }
 
 initDB();
